90ecd49340762aa462c6f8b3bcf7b9fdbc9ee81b
Rúben Filipe <rubenfilipe.net@gmail.com>
2022-07-12 04:30:10 -0300

    sending files

--- App.js
+++ App.js
@@ -1,10 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View} from 'react-native';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { BlocoMoeda } from './blocoMoeda';
 
 export default function App() {
+ 
+  const [dolar, setDolar] = useState();
+  const [euro, setEuro]   = useState();
+  const [libra, setLibra] = useState();
+  const [iene, setIene]   = useState();
+  const [dolarAustraliano, setDolarAustraliano] = useState();
+  const [francoSuico, setFrancoSuico] = useState();
+  const [dolarCanadense, setDolarCanadense] = useState();
+  const [yuan, setYuan] = useState();
+  const [pesoArgentino, setPesoArgentino] = useState();
+  
+  const getMonetary  = () => {
+    axios.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL,JPY-BRL,AUD-BRL,CHF-BRL,CAD-BRL,CNY-BRL,ARS-BRL')
+    .then(res => {
+      handlerMonetary(res.data);
+    } ) .catch(err => { console.log(err) }  );
+  }
+
+
+  useEffect(() => {
+   getMonetary();
+  }, []);
+  
+
+  const handlerMonetary = (data) => {
+    setDolar(parseFloat(data.USDBRL.bid).toFixed(2));
+    setEuro(parseFloat(data.EURBRL.bid).toFixed(2));
+    setLibra(parseFloat(data.GBPBRL.bid).toFixed(2));
+    setIene(parseFloat(data.JPYBRL.bid).toFixed(2));
+    setDolarAustraliano(parseFloat(data.AUDBRL.bid).toFixed(2));
+    setFrancoSuico(parseFloat(data.CHFBRL.bid).toFixed(2));
+    setDolarCanadense(parseFloat(data.CADBRL.bid).toFixed(2));
+    setYuan(parseFloat(data.CNYBRL.bid).toFixed(2));
+    setPesoArgentino(parseFloat(data.ARSBRL.bid).toFixed(2));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <BlocoMoeda simbolo={'$'} titulo='Dólar' moeda={dolar} corFundo="#e8e7d0" corSimbolo="#13391a"/>
+      <BlocoMoeda simbolo={'€'} titulo='Euro' moeda={euro} corFundo="#CAADCC" corSimbolo="#533F75"/>
+      <BlocoMoeda simbolo={'£'} titulo='Libra' moeda={libra} corFundo="#F1E5CF" corSimbolo="#C61635"/>
+      <BlocoMoeda simbolo={'¥'} titulo='Iene' moeda={iene} corFundo="#E2CAB2" corSimbolo="#45434C"/>
+      <BlocoMoeda simbolo={'A$'} titulo='Dólar Australiano' moeda={dolarAustraliano} corFundo="#76CBA0" corSimbolo="#006482"/>
+      <BlocoMoeda simbolo={'Fr'} titulo='Franco Suíco' moeda={francoSuico} corFundo="#AFA2CD" corSimbolo="#7E5596"/>
+      <BlocoMoeda simbolo={'C$'} titulo='Dólar Canadense' moeda={dolarCanadense} corFundo="#7EA785" corSimbolo="#365946"/>
+      <BlocoMoeda simbolo={'¥'} titulo='Remibi (Yuan)' moeda={yuan} corFundo="#D5C7C6" corSimbolo="#C88177"/>
+      <BlocoMoeda simbolo={'$'} titulo='Peso Argentino' moeda={pesoArgentino} corFundo="#4A4680" corSimbolo="#C5D8E7"/>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +62,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#21262d',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    paddingTop: 30,
+  }
 });
---
+++ blocoMoeda.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+
+export  function BlocoMoeda(props) {
+  
+  return (
+        <View style={styles.bloco}>
+            <View style={styles.blocoSimbolo}>
+                <View style={[styles.blocoSimboloMoeda, {backgroundColor: props.corFundo}]}>
+                    <Text style={[styles.simboloMoeda, {color: props.corSimbolo}]}>{props.simbolo}</Text>
+                </View>
+
+                <View style={styles.blocoMoeda}>
+                    <Text style={styles.tituloMoeda}>{props.titulo}</Text>
+                    <Text style={styles.valorMoeda}>R$ {props.moeda}</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    bloco: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      width: '100%',
+      height: '10%',
+      borderBottomWidth: 1,
+      borderBottomColor: '#303336',
+      width: '80%',
+    },
+    blocoSimbolo: {
+      flexDirection: 'row',
+      display: 'flex',
+    },
+    blocoMoeda: {
+      flexDirection: 'column',
+      display: 'flex',
+      marginLeft: 10,
+      marginRight: 10,
+      padding: 0,
+      marginTop: 0,
+      marginBottom: 0,
+    },
+    blocoSimboloMoeda: {
+      width: 50,
+      height: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 10,
+    },
+    simboloMoeda: {
+      fontSize: 30,
+      fontWeight: '600',
+    },
+    tituloMoeda: {
+      fontSize: 30,
+      marginTop: -8,
+      color: '#efefef',
+    }
+    ,
+    valorMoeda: {
+      fontSize: 20,
+      color: '#c2cdd3',
+    },
+  });
+  
